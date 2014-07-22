5bae141b581720898b486a641171fbcd076069d0
Aaron Pang <pang.aaron56@gmail.com>
2014-07-22 13:07:14 -0400

    fuckin added this fuckin shit

--- prototype/prototype/MainViewController.m
+++ prototype/prototype/MainViewController.m
@@ -18,10 +18,26 @@
 
 -(void)viewDidLoad {
     [super viewDidLoad];
+    CGRect screenRect = [[UIScreen mainScreen] bounds];
+    CGFloat screenWidth = screenRect.size.height;
+    CGFloat screenHeight = screenRect.size.width;
     [self.view setBackgroundColor:[UIColor redColor]];
     
-    NSString *path = [[NSBundle mainBundle] pathForResource:@"testVideo" ofType:@"mp4"];
+    // Add the three views
+    _leftPanelView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 300, screenHeight)];
+    _leftPanelView.backgroundColor = [UIColor blueColor];
+    [self.view addSubview:_leftPanelView];
     
+    _timelineView = [[UIView alloc] initWithFrame:CGRectMake(_leftPanelView.frame.size.width, screenHeight / 2, screenWidth - _leftPanelView.frame.size.width, screenHeight / 2)];
+    _timelineView.backgroundColor = [UIColor yellowColor];
+    [self.view addSubview:_timelineView];
+    
+    _videoPreviewView = [[UIView alloc] initWithFrame:CGRectMake(_leftPanelView.frame.size.width, 0, screenWidth - _leftPanelView.frame.size.width, screenHeight / 2)];
+    _videoPreviewView.backgroundColor = [UIColor purpleColor];
+    [self.view addSubview:_videoPreviewView];
+    
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"testVideo" ofType:@"mp4"];
+    // Add the video player
     _myPlayer = [[MPMoviePlayerController alloc] initWithContentURL:[NSURL fileURLWithPath:path]];
     _myPlayer.shouldAutoplay = NO;
     _myPlayer.repeatMode = MPMovieRepeatModeOne;
@@ -29,8 +45,8 @@
     _myPlayer.controlStyle = MPMovieControlStyleNone;
     _myPlayer.scalingMode = MPMovieScalingModeFill;
     [_myPlayer prepareToPlay];
-    _myPlayer.view.frame = CGRectMake(550, 70, 300, 250);
-    [self.view addSubview:_myPlayer.view];
+    _myPlayer.view.frame = CGRectMake(100, 50, _videoPreviewView.frame.size.width - 200, _videoPreviewView.frame.size.height - 100);
+    [_videoPreviewView addSubview:_myPlayer.view];
     [_myPlayer play];
 }
 
