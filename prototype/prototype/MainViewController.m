c8ae0868da4190606e7aa54e99b33e39940607aa
Aaron Pang <pang.aaron56@gmail.com>
2014-07-22 12:35:22 -0400

    Added movie player video thing

--- prototype/prototype/MainViewController.m
+++ prototype/prototype/MainViewController.m
@@ -7,14 +7,31 @@
 //
 
 #import "MainViewController.h"
+#import <MediaPlayer/MediaPlayer.h>
 
 @implementation MainViewController {
-    
+    UIView *_leftPanelView;
+    UIView *_videoPreviewView;
+    UIView *_timelineView;
+    MPMoviePlayerController *_myPlayer;
 }
 
 -(void)viewDidLoad {
     [super viewDidLoad];
     [self.view setBackgroundColor:[UIColor redColor]];
+    
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"testVideo" ofType:@"mp4"];
+    
+    _myPlayer = [[MPMoviePlayerController alloc] initWithContentURL:[NSURL fileURLWithPath:path]];
+    _myPlayer.shouldAutoplay = NO;
+    _myPlayer.repeatMode = MPMovieRepeatModeOne;
+    _myPlayer.movieSourceType = MPMovieSourceTypeFile;
+    _myPlayer.controlStyle = MPMovieControlStyleNone;
+    _myPlayer.scalingMode = MPMovieScalingModeFill;
+    [_myPlayer prepareToPlay];
+    _myPlayer.view.frame = CGRectMake(550, 70, 300, 250);
+    [self.view addSubview:_myPlayer.view];
+    [_myPlayer play];
 }
 
 @end
